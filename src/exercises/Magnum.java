056b8d0c4a97bdbe156c3189681f045129f4302b
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-11-23 18:49:43 -0600

    exercise 2.4.2 part 1 complete

---
+++ src/exercises/Magnum.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+
+public class Magnum {
+    public static void main(String[] args) {
+        double length;
+        double width;
+        double area;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of your rectangle?");
+        length = input.nextDouble();
+        System.out.println("What is the width of your rectangle?");
+        width = input.nextDouble();
+        input.close();
+
+        area = length * width;
+        System.out.println("The area of your rectangle is: 5" + area);
+    }
+}
