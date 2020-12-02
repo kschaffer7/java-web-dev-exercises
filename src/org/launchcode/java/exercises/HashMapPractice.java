94edaefba3d15835d495b54eab519de268ed1e84
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-12-02 17:34:07 -0600

    hash map practice complete

---
+++ src/org/launchcode/java/exercises/HashMapPractice.java
@@ -0,0 +1,41 @@
+package org.launchcode.java.exercises;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashMapPractice {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        // Get student names and grades
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID Number: ");
+                int idNumber = input.nextInt();
+                students.put(idNumber, newStudent);
+
+                // Read in the newline before looping back (this is another solution similar to input.skip("\n")
+                input.nextLine();
+            }
+
+        } while(!newStudent.equals(""));
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+        }
+    }
+}
