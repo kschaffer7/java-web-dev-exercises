6aae287eccb8f7c44cce517a3e8f991b9d171b89
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-12-02 19:47:24 -0600

    tweaks to arraylist

--- src/org/launchcode/java/exercises/ArrayListPractice.java
+++ src/org/launchcode/java/exercises/ArrayListPractice.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 
 public class ArrayListPractice {
     public static void main(String[] args){
-        ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(4, 5, 6, 2, 8, 12, 64, 63, 23, 22));
+        // Within main, create a list with at least 10 integers and call your method on the list.
+        ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 5, 4, 3, 2, 1));
         System.out.println(sumEvens(listOfNumbers));
-
+        //Modify your code to prompt the user to enter the word length for the search.
         Scanner input = new Scanner(System.in);
         System.out.println("How long should the words be?");
         int length = input.nextInt();
         input.skip("\n");
-        ArrayList<String> listOfWords = new ArrayList<>(Arrays.asList("Hello", "There", "This", "Pinky", "Throw"));
+        ArrayList<String> listOfWords = new ArrayList<>(Arrays.asList("I", "Bo", "am", "a", "Mamas", "boy"));
         print5LetterWords(listOfWords, length);
         input.close();
     }
-
+    // Write a static method to find the sum of all the even numbers in an ArrayList.
     public static int sumEvens(ArrayList<Integer> numbers){
         int total = 0;
         for(int number: numbers){
@@ -27,7 +28,8 @@ public class ArrayListPractice {
         }
         return total;
     }
-
+    // Write a static method to print out each word in a list that has exactly 5 letters.
+    //Modify your code to prompt the user to enter the word length for the search.
     public static void print5LetterWords(ArrayList<String> words, int length){
         for(String word: words){
             if(word.length() == length){
