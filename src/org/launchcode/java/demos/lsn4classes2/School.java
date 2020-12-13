909e30efd830a1404965ec8895174508c64bbd4c
Kirsti Schaffer <kirstischaffer@gmail.com>
2020-12-12 18:33:16 -0600

    classes 2 exercises updates

--- src/org/launchcode/java/demos/lsn4classes2/School.java
+++ src/org/launchcode/java/demos/lsn4classes2/School.java
@@ -1,15 +1,18 @@
-//package org.launchcode.java.demos.lsn4classes2;
-//
-//public class School {
-//    public static void main(String[] args){
-//        Student bo = new Student("Bo");
-//        bo.setNumberofCredits(67);
-//        bo.getGpa(3.8);
-//        System.out.println(bo);
-//
-//        Student boBo = new Student("Bo");
-//        bo.setNumberofCredits(67);
-//        bo.getGpa(3.8);
-//        System.out.println(boBo);
-//    }
-//}
+package org.launchcode.java.demos.lsn4classes2;
+
+import java.util.Scanner;
+
+public class School {
+    public static void main(String[] args){
+        Student bo = new Student("Bo");
+        bo.setNumberOfCredits(67);
+        bo.setGpa(3.9);
+        System.out.println(bo);
+
+        Student bo2 = new Student("Bo");
+        Scanner input = new Scanner(System.in);
+        bo2.setNumberOfCredits(67);
+        bo2.setGpa(3.9);
+        System.out.println(bo.equals(bo2));
+    }
+}
--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,110 +1,123 @@
-//package org.launchcode.java.demos.lsn4classes2;
-//
-//public class Student {
-//
-//    private static int nextStudentId = 1;
-//    private String name;
-//    private int studentId;
-//    private int numberOfCredits = 0;
-//    private double gpa = 0.0;
-//
-//    public Student (String name, int studentId, int numberOfCredits, double gpa) {
-//        this.name = name;
-//        this.studentId = studentId;
-//        this.numberOfCredits = numberOfCredits;
-//        this.gpa = gpa;
-//    }
-//
-//    public Student(String name, int studentId) {
-//        this(name, studentId, 0, 0);
-//    }
-//
-//    public Student(String name) {
-//        this(name, nextStudentId);
-//        nextStudentId++;
-//    }
-//
-//    public String studentInfo() {
-//        return (this.name + " has a GPA of: " + this.gpa);
-//    }
-//
-//
-//     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//        if(this.numberOfCredits <= 29){
-//            return "Freshman";
-//        } else if(this.numberOfCredits >= 30 && this.numberOfCredits <= 59){
-//            return "Sophomore";
-//        } else if(this.numberOfCredits <= 89){
-//            return "Junior";
-//        } else {
-//            return "Senior";
-//        }
-//    }
-//
-//    // TODO: Complete the addGrade method.
-//    public void addGrade(int courseCredits, double grade) {
-//        // Update the appropriate fields: numberOfCredits, gpa
-//        this.numberOfCredits += courseCredits;
-//        double courseQualityScore = courseCredits * grade;
-//        double overallQualityScore = this.numberOfCredits * this.getGpa();
-//        this.gpa = (courseQualityScore + overallQualityScore)/this.numberOfCredits;
-//    }
-//
-//    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
-//    //  than just the class fields.
-//    @Override
-//    public String toString() {
-//        return "Name:" + this.name + ", ID: " + this.studentId;
-//    }
-//
-//    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
-//    //  Student objects equal.
-//    @Override
-////    public boolean equals(Object toBeCompared){
-////        Student student2 = (Student) toBeCompared;
-////        return this.gpa == student2.gpa;
-////    }
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public int getStudentId() {
-//        return studentId;
-//    }
-//
-//    public int getNumberOfCredits() {
-//        return numberOfCredits;
-//    }
-//
-//    public double getGpa() {
-//        return gpa;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public void setStudentId(int studentId) {
-//        this.studentId = studentId;
-//    }
-//
-//    public void setGpa(double gpa) {
-//        this.gpa = gpa;
-//    }
-//
-//    private void setNumberOfCredits(int numberOfCredits) {
-//        this.numberOfCredits = numberOfCredits;
-//    }
-//
-//    public static void main(String[] args) {
-//        Student sally = new Student("Sally",1,1,4.0);
-//        System.out.println("The Student class works! " + sally.getName() + " is a student!");
-//        System.out.println(sally);
-//        sally.addGrade(12, 3.5);
-//        System.out.println(sally);
-//        sally.addGrade(25, 3.8);
-//        System.out.println(sally);
-//    }
-//}
+package org.launchcode.java.demos.lsn4classes2;
+
+public class Student {
+
+    private static int nextStudentId = 1;
+    private String name;
+    private int studentId;
+    private int numberOfCredits = 0;
+    private double gpa = 0.0;
+
+    public Student (String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+
+     //TODO: Uncomment and complete the getGradeLevel method here:
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        if(this.numberOfCredits <= 29){
+            return "Freshman";
+        } else if(this.numberOfCredits <= 59){
+            return "Sophomore";
+        } else if(this.numberOfCredits <= 89){
+            return "Junior";
+        } else {
+            return "Senior";
+        }
+    }
+
+    // TODO: Complete the addGrade method.
+    public void addGrade(int courseCredits, double grade) {
+        // Update the appropriate fields: numberOfCredits, gpa
+        this.numberOfCredits += courseCredits;
+        double courseQualityScore = courseCredits * grade;
+        double overallQualityScore = this.numberOfCredits * this.getGpa();
+        this.gpa = (courseQualityScore + overallQualityScore)/this.numberOfCredits;
+    }
+
+    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
+    //  than just the class fields.
+    @Override
+    public String toString() {
+        return "Name:" + this.name + ", ID: " + this.studentId;
+    }
+
+    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
+    //  Student objects equal.
+    @Override
+    public boolean equals(Object toBeCompared){
+        // are the objects the same object
+        if(toBeCompared == this){
+            System.out.println("They are the same!");
+            return true;
+        }
+        // is the object null
+        if(toBeCompared == null){
+            return false;
+        }
+        // are the classes the same
+        if(toBeCompared.getClass() != this.getClass()){
+            return false;
+        }
+        Student student2 = (Student) toBeCompared;
+        return this.gpa == student2.gpa;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public static void main(String[] args) {
+        Student sally = new Student("Sally",1,1,4.0);
+        System.out.println("The Student class works! " + sally.getName() + " is a student!");
+        System.out.println(sally);
+        sally.addGrade(12, 3.5);
+        System.out.println(sally);
+        sally.addGrade(25, 3.8);
+        System.out.println(sally);
+    }
+}
